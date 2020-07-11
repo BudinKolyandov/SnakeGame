38d6526dd6b55a8ffaa024d0b45604ea5c8990e9
Budin Kolyandov <budin_kolyandov@abv.bg>
2020-07-11 19:06:53 +0300

    Add project files.

---
+++ SnakeGame/StartUp.cs
@@ -0,0 +1,19 @@
+﻿namespace SnakeGame
+{
+    using SnakeGame.GameObjects;
+    using SnakeGame.Utilities;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            ConsoleSettings.CustomizeConsole();
+
+            Wall wall = new Wall(60, 20);
+            Snake snake = new Snake(wall);
+
+            Engine engine = new Engine(wall, snake);
+            engine.Run();
+        }
+    }
+}
